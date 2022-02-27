4c12c90508ccfe056b719fd4d492663b736e9ad1
Mukul Saini <contact.mukul.saini@gmail.com>
2022-02-27 18:58:29 +0530

    Setting up Datomic dev-local and quering db

--- project.clj
+++ project.clj
@@ -3,5 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
-  :repl-options {:init-ns ddl.core})
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [com.datomic/dev-local "1.0.242"]]
+  :repl-options {:init-ns ddl.core}
+
+  :repositories [["cognitect-dev-tools" {:url      "https://dev-tools.cognitect.com/maven/releases/"
+                                         :username :env
+                                         :password :env}]])
--- src/ddl/core.clj
+++ src/ddl/core.clj
@@ -1,6 +1,92 @@
-(ns ddl.core)
+(ns ddl.core
+  (:require [datomic.client.api :as d]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+
+(comment
+
+  ;; Refer the the Datomic docs for configuring/setting it up
+  ;; https://docs.datomic.com/cloud/dev-local.html#getting-started
+  ;; https://docs.datomic.com/cloud/tutorial/client.html#create-database 
+
+  ;;Creating datamic client
+  (def client (d/client {:server-type :dev-local
+                         :system "datomic-samples"}))
+
+  ;;Reading databases
+  (d/list-databases client {})
+
+  ;;Create a database
+  (d/create-database client {:db-name "movies-new"})
+
+  ;;Establish connection with your database
+  (def conn (d/connect client {:db-name "movies-new"}))
+
+  ;;Define schema of your DB
+  (def movie-schema [{:db/ident :movie/title
+                      :db/valueType :db.type/string
+                      :db/cardinality :db.cardinality/one
+                      :db/doc "The title of the movie"}
+
+                     {:db/ident :movie/genre
+                      :db/valueType :db.type/string
+                      :db/cardinality :db.cardinality/one
+                      :db/doc "The genre of the movie"}
+
+                     {:db/ident :movie/release-year
+                      :db/valueType :db.type/long
+                      :db/cardinality :db.cardinality/one
+                      :db/doc "The year the movie was released in theaters"}])
+
+  ;;Transacting schema in our db.
+  (d/transact conn {:tx-data movie-schema})
+
+  ;;Defining a set of movies to add to DB
+  (def first-movies [{:movie/title "The Goonies"
+                      :movie/genre "action/adventure"
+                      :movie/release-year 1985}
+                     {:movie/title "Commando"
+                      :movie/genre "thriller/action"
+                      :movie/release-year 1985}
+                     {:movie/title "Repo Man"
+                      :movie/genre "punk dystopia"
+                      :movie/release-year 1984}])
+
+  ;;Transct the movies data to add it to DB
+  (d/transact conn {:tx-data first-movies})    ;; Now you DB contains the data of movies as per the schema you defined
+
+  ;;Define reference to db
+  (def db (d/db conn))
+
+  
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;Write query to fech movie titles data from DB
+  (def all-titles-q '[:find ?movie-title
+                      :where [_ :movie/title ?movie-title]])
+
+  ;;Quering DB for the movie title's data
+  (d/q all-titles-q db)
+  
+  ;;Find movie genre
+  (d/q '[:find ?movie-genre
+        :where [_ :movie/genre ?movie-genre]] db)
+  
+  ;;Find movie release year
+  (d/q '[:find ?movie-year
+         :where [_ :movie/release-year ?movie-year]] db)
+  
+  ;;Find movie title's that were relesae in year 1984
+  (d/q '[:find ?movie-title
+         :where
+         [?m :movie/release-year 1984]
+         [?m :movie/title ?movie-title]] db)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  
+  ;;Drop your databse
+  (d/delete-database client {:db-name "movies-new"})
+  )
