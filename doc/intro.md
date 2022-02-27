b073beb360f6416b992485c103d4916ef5d1dd94
Mukul Saini <contact.mukul.saini@gmail.com>
2022-02-27 16:05:24 +0530

    Default lein template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ddl
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ddl/core_test.clj
@@ -0,0 +1,7 @@
+(ns ddl.core-test
+  (:require [clojure.test :refer :all]
+            [ddl.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
